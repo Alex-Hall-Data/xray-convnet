14e8a3462c645b91fdd799f871902724ad98bff4
alex hall <alex.hall@outplay.com>
2018-01-26 15:56:26 +0000

    initial commit

---
+++ one_hot_encoder.R
@@ -0,0 +1,5 @@
+#script to change the classifications to one hot encoding
+
+xray_data <- read.csv("C:\\Users\\alex.hall\\Documents\\datasets\\xray\\Data_Entry_2017.csv\\Data_Entry_2017.csv")
+
+class_list<-c()
