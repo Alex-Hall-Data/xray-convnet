35ed53010745ab57dd14586a98c31e2b9ba48c9c
alex hall <alex.hall@outplay.com>
2018-01-27 00:07:24 +0000

    Completed data import and manipulation

--- one_hot_encoder.R
+++ one_hot_encoder.R
@@ -2,4 +2,21 @@
 
 xray_data <- read.csv("C:\\Users\\alex.hall\\Documents\\datasets\\xray\\Data_Entry_2017.csv\\Data_Entry_2017.csv")
 
-class_list<-c()
+labels_list <- paste(xray_data$Finding.Labels,collapse="|")%>%
+  strsplit(.,"\\|")
+
+unique_labels<-unique(as.vector(labels_list)[[1]])
+
+#initialise empty list of vectors (zeros)
+labels<-list()
+for (i in(1:nrow(xray_data))){
+  labels[i]<-list(rep(0,length(unique_labels)))
+}
+
+#make list of k hot encoded vectors
+for(i in (1:length(labels))){
+  for(j in (1: length(labels[[i]]))){
+    labels[[i]][j]<- ifelse(grepl(unique_labels[j],xray_data$Finding.Labels[i]),1,0)
+  }
+  print(i)
+}
